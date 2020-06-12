fee7af47152f4544c4c2e1e9a2be0f4e11193cad
jerome <jerome@jerome.sgic.com>
2020-06-12 14:26:40 +0530

    clean up and typescript modification for app.js done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props{}
+
+interface State{}
+
+class App extends React.Component<Props,State> {
+  constructor(props:Props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+
+  render() {
+    return (
+      <div>
+        <p>Hi</p>
+      </div>
+    );
+  }
 }
 
 export default App;
