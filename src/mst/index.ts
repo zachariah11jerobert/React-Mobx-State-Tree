dcb59cdffc0c22520e72122154f18d88e36252b3
jerome <jerome@jerome.sgic.com>
2020-06-12 19:05:01 +0530

    store and model created

---
+++ src/mst/index.ts
@@ -0,0 +1,24 @@
+import {types,Instance} from "mobx-state-tree";
+
+const EmployeeModel =types.model("Employee",{
+    id:types.identifier,
+    name:types.string,
+    hours_worked:types.number
+});
+
+const EmployerModel =types.model("Employer",{
+    id:types.identifier,
+    name:types.string,
+    location:types.string,
+    employees:types.array(EmployeeModel)
+});
+
+const RootModel =types.model("Root",{
+    employer:EmployerModel
+});
+
+export {RootModel};
+
+export type Root = Instance<typeof RootModel>;
+export type Employer = Instance<typeof EmployerModel>;
+export type Employee = Instance<typeof EmployeeModel>;
